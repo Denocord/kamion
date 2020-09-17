4b07707cdd66f3497d718147433767902012610c
TTtie <tttiett@outlook.com>
2020-09-17 17:20:14 +0200

    🔊 Make the operations noisier

--- src/check.sh
+++ src/check.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 . $KAMION_BASE/util/functions.sh
 
-mkdir update
-cd update
-git init
+echo "Initializing git repository"
 git config --global pull.rebase false
 git config --global user.name Kamion
 git config --global user.email kamion@tttie.local
-git remote add origin $CURRENT_REPO
+
+echo "Cloning $OUTPUT_BRANCH at $CURRENT_REPO..."
+git clone -b $OUTPUT_BRANCH $CURRENT_REPO update
+cd update
 git remote add upstream $UPSTREAM_REPO
 
+echo "Fetching new commits from $UPSTREAM_REPO..."
 git fetch upstream
-git fetch origin
 
 export UPSTREAM_REF=$(git rev-parse upstream/$UPDATE_BRANCH)
 export CURRENT_REF=$(git rev-parse origin/$OUTPUT_BRANCH)
@@ -21,11 +22,8 @@ if [[ $UPSTREAM_REF == $CURRENT_REF ]]; then
     exit 0
 fi
 
-
-git pull origin $CURRENT_BRANCH
-
 if [[ -n $UPDATE_BRANCH ]]; then
-    UPDATE_BRANCH=$CURRENT_BRANCH
+    UPDATE_BRANCH=$OUTPUT_BRANCH
 fi
 
 ARGS=""
@@ -34,14 +32,17 @@ if [[ $UPDATE_METHOD == "rebase" ]]; then
     ARGS="--rebase"
 fi
 
+echo "Pulling in the updates from $UPDATE_BRANCH at $UPSTREAM_REPO..."
 git pull $ARGS upstream $UPDATE_BRANCH
 
+echo "Pushing the new commits to $CURRENT_REPO..."
 git push -u origin $OUTPUT_BRANCH
 
 OUTPUT_SCRIPT="$KAMION_BASE/outputs/$KAMION_OUTPUT.sh"
 IS_VALID_DIRECTORY=$(test_directory $OUTPUT_SCRIPT "$KAMION_BASE/outputs")
 
 if [[ $IS_VALID_DIRECTORY == 0 ]]; then
+    echo "Running the $KAMION_OUTPUT output script..."
     . $OUTPUT_SCRIPT
 fi
 
