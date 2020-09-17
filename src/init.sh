d174b09f36e99b01e7e9884be3a019d3e7fcb588
TTtie <tttiett@outlook.com>
2020-09-17 16:51:13 +0200

    🐛 (init): correct the script location

--- src/init.sh
+++ src/init.sh
@@ -2,6 +2,6 @@
 KAMION_REPO="https://github.com/Denocord/kamion.git"
 git clone $KAMION_REPO kamion
 
-export KAMION_BASE="$PWD/kamion"
+export KAMION_BASE="$PWD/kamion/src"
 
 . $KAMION_BASE/check.sh
