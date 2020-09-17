0c6e38e2709d75bd1ca9148645f76e38451fcc7e
TTtie <tttiett@outlook.com>
2020-09-17 19:30:38 +0200

    🐛 (outputs/github): remove .git from repo URL

--- src/outputs/github.sh
+++ src/outputs/github.sh
@@ -2,7 +2,8 @@
 GITHUB_API_BASE="https://api.github.com"
 
 PR_NAME="Update $UPSTREAM_REPO to $UPSTREAM_REF"
-PR_DESCRIPTION="This pull request updates $UPSTREAM_REPO to $UPSTREAM_REPO/commits/$UPSTREAM_REF."
+UPSTREAM_REPO_URL=$(echo "$UPSTREAM_REPO" | sed -e "s/.git$//")
+PR_DESCRIPTION="This pull request updates $UPSTREAM_REPO to $UPSTREAM_REPO_URL/commits/$UPSTREAM_REF."
 PR_BASE_URL="$GITHUB_API_BASE/repos/$GITHUB_REPO_SLUG/pulls"
 
 PR_ID=$(curl -fsSL -H "Authorization: Bearer $GITHUB_TOKEN" "$PR_BASE_URL?head=$UPDATE_BRANCH" | jq -e ".[0].number")
