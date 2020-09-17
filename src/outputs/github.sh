ea0205a17d60ae9d43113c21c9fd4555a2df785d
TTtie <tttiett@outlook.com>
2020-09-17 18:44:11 +0200

    🐛 (outputs/github): explicitly specify curl args

--- src/outputs/github.sh
+++ src/outputs/github.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-CURL_ARGS="-fsSL -H 'Authorization: Bearer $GITHUB_TOKEN' -o -"
+CURL_ARGS="-fsSL "
 GITHUB_API_BASE="https://api.github.com/"
 
 PR_NAME="Update $UPSTREAM_REPO to $UPSTREAM_REF"
 PR_DESCRIPTION="This pull request updates $UPSTREAM_REPO to $UPSTREAM_REF/commits/$UPSTREAM_REF."
 PR_BASE_URL="$GITHUB_API_BASE/repos/$GITHUB_REPO_SLUG/pulls"
 
-PR_ID=$(curl $CURL_ARGS "$PR_BASE_URL?head=$UPDATE_BRANCH" | jq -e ".[0].number")
+PR_ID=$(curl -fsSL -H "Authorization: Bearer $GITHUB_TOKEN" "$PR_BASE_URL?head=$UPDATE_BRANCH" | jq -e ".[0].number")
 
 if [[ $? == 0 ]]; then
     echo "An update pull request exists already - updating the PR label accordingly."
@@ -18,7 +18,7 @@ if [[ $? == 0 ]]; then
         --arg prBody $PR_DESCRIPTION \
         '{ title: $prName, body: $prBody }'
     )
-    curl $CURL_ARGS -X "PATCH" -d $REQUEST_BODY $PR_BASE_URL/$PR_ID 
+    curl -fsSL -H "Authorization: Bearer $GITHUB_TOKEN" -X "PATCH" -d $REQUEST_BODY $PR_BASE_URL/$PR_ID 
 fi
 
 REQUEST_BODY=$(
@@ -29,4 +29,4 @@ REQUEST_BODY=$(
     '{ title: $prName, body: $prBody, head: $prHead }'
 )
 
-curl $CURL_ARGS -X "POST" -d $REQUEST_BODY $PR_BASE_URL
+curl -fsSL -H "Authorization: Bearer $GITHUB_TOKEN" -X "POST" -d $REQUEST_BODY $PR_BASE_URL
