c75fd3faee84995d8b0a8efdcf3dac65c94d6dcb
TTtie <me@tttie.cz>
2020-11-03 20:02:53 +0100

    🔥 remove test script

--- test.sh
+++
@@ -1,9 +0,0 @@
-UPSTREAM_REF="a"
-UPSTREAM_REPO="http://localhost"
-PR_NAME="Update $UPSTREAM_REPO to $UPSTREAM_REF"
-PR_DESCRIPTION="This pull request updates $UPSTREAM_REPO to $UPSTREAM_REF/commits/$UPSTREAM_REF."
-
-jq -n \
-    --arg prName "$PR_NAME" \
-    --arg prBody "$PR_DESCRIPTION" \
-    '{title: $prName,body: $prBody}'
